33fbf61c2e0c0bbb93e5865db6ce4bbcffc66adf
albertcraiova <munteanu.albert97@yahoo.com>
2021-02-15 23:57:21 +0200

    My first Commit

---
+++ src/components/Translate.js
@@ -0,0 +1,61 @@
+import React, {useState} from  'react'
+import Dropdown from './Dropdown'
+import Convert from './Convert'
+
+const options = [  
+    {
+        label: 'Czech',
+        value: 'cs',
+    },
+    {
+        label: 'Arabic',
+        value: 'ar',
+    },
+    {
+        label: 'Dutch',
+        value: 'nl',
+    },
+    {
+        label: 'Romanian',
+        value: 'ro',
+    },
+    {
+        label: 'English',
+        value: 'en',
+    },
+    {
+        label: 'French',
+        value: 'fr',
+    },
+    {
+        label: 'Italian',
+        value: 'it',
+    }
+]
+
+const Translate =() => {
+    const [language, setLanguage] = useState(options[0])
+    const [text,setText] = useState('')
+    
+    return(
+        <div>
+            <div className="ui form">
+              <div className="field">
+                <label>Enter Text</label>
+                <input value={text} onChange={(e) => setText(e.target.value)} />
+              </div>
+            </div>
+            <Dropdown 
+              label="Select a Language"
+              selected={language} 
+              onSelectedChange={setLanguage} 
+              options={options}
+            />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
+        </div>
+    )
+}
+
+export default Translate
